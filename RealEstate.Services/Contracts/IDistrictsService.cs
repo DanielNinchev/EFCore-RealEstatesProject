964ad346702a7d8dd607dc90c17601661bb6c864
Daniel Ninchev <daniel.ninchev@gmail.com>
2021-04-30 14:05:24 +0300

    Implement services and RealEstates.Importer. Add Initial migration.

---
+++ RealEstate.Services/Contracts/IDistrictsService.cs
@@ -0,0 +1,14 @@
+﻿using RealEstates.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services.Contracts
+{
+    public interface IDistrictsService
+    {
+        IEnumerable<DistrictViewModel> GetTopDistrictsByAveragePrice(int count = 10);
+
+        IEnumerable<DistrictViewModel> GetTopDistrictsByNumberOfProperties(int count = 10);
+    }
+}
--- RealEstates.ConsoleApplication/Program.cs
+++ RealEstates.ConsoleApplication/Program.cs
@@ -1,5 +1,9 @@
-﻿using RealEstates.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+using RealEstates.Services;
+using RealEstates.Services.Contracts;
 using System;
+using System.Text;
 
 namespace RealEstates.ConsoleApplication
 {
@@ -7,8 +11,19 @@ namespace RealEstates.ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             var db = new RealEstateDbContext();
-            db.Database.EnsureCreated();
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+
+            IDistrictsService districtsService = new DistrictService(db);
+            var districts = districtsService.GetTopDistrictsByAveragePrice();
+
+            foreach (var district in districts)
+            {
+                Console.WriteLine($"{district.Name} => Price: {district.AveragePrice:0.00} ({district.MinPrice}-{district.MaxPrice}) => {district.PropertiesCount} properties");
+            }
         }
     }
 }
